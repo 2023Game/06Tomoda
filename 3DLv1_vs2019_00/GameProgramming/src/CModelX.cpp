b91e817c44027a712f2f79b9107c91fa6503339d
Tomoda-Yuki-Fukuyama <yuki_chan_0222@icloud.com>
2024-04-17 12:24:52 +0900

    2

--- 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -43,12 +43,100 @@ void CModelX::Load(char* file)
 	//最後に\0を設定する（文字列の終端）
 	buf[size] = '\0';
 
-	printf("%s\n", buf);
+	//文字列の最後まで繰り返し
+	while (*mpPointer != '\0')
+	{
+		GetToken();   //単語の取得
+		//単語がFrameの場合
+		if (strcmp(mToken, "Frame") == 0)
+		{
+			printf("%s ", mToken);  //Freme出力
+			GetToken();            //Freme名を取得
+			printf("%s\n", mToken);//Freme名を出力
+		}
 
-	fclose(fp);          //ファイルをクローズする
+		if (strcmp(mToken, "AnimationSet") == 0)
+		{
+			printf("%s ", mToken);  
+			GetToken();            
+			printf("%s\n", mToken);
+		}
+	}
 
 	SAFE_DELETE_ARRAY(buf);  //確保した領域を開放する
 
-	
+
+	fclose(fp);          //ファイルをクローズする
+
 }
 
+char* CModelX::GetToken()
+{
+	char* p = mpPointer;
+	char* q = mToken;
+
+	//タブ(\t)空白()改行(\r)(\n),;”の区切り文字以外になるまで読み飛ばす
+	while (*p != '\0' && IsDelimiter(*p)) p++;
+	if (*p == '{' || *p == '}')
+	{
+		//{または}ならmTokenに代入し次の文字へ
+		*q++ = *p++;
+	}
+	else {
+		//タブ(\t)空白()改行(\r)(\n),;"の区切り文字、
+		//または、}の文字になるまでmTokenに代入する
+		while (*p != '\0' && !IsDelimiter(*p) && *p != '}')
+			*q++ = *p++;
+	}
+
+	*q = '\0';      //mTokenの最後に\0を代入
+	mpPointer = p;  //次の読み込むポイントを更新する
+
+	//もしmTokenが//の場合は、コメントなので改行まで読み飛ばす
+	/*
+	strcmp(文字列1,文字列2)
+	文字列1と文字列2が等しい場合、0を返す
+	文字列1と文字列2が等しくない場合、0以外を返す
+	*/
+
+	if (!strcmp("//", mToken))
+	{
+		//改行まで読み飛ばす
+		while (*p != '\0' && !strchr("\r\n", * p)) p++;
+
+		//読み込み位置の更新
+		mpPointer = p;
+
+		//単語を取得する(再起呼び出し)
+		return GetToken();
+	}
+	return mToken;
+}
+
+#include <ctype.h>
+/*
+*IsDelimiter(c)
+*cが\t\r\nスペースなどの空白文字
+*または,;"などの文字であれば
+*区切り文字としてtrueを返す
+*/
+
+bool CModelX::IsDelimiter(char c)
+{
+	//isspace(c)
+	//cが空白文字なら0以外を返す
+	if (isspace(c) != 0)
+		return true;
+
+	/*
+	strchr(文字列,文字)
+	文字列に文字が含まれていれば、
+	見つかった文字へのポインタを返す
+	見つからなかったらNULLを返す
+	*/
+	if (strchr(",;\"", c) != NULL)
+		return true;
+
+	//区切り文字ではない
+	return false;
+}
